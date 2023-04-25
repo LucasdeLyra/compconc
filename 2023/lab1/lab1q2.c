f926cb34a68cc8ef10a6d52a0a30aaa16b60ee15
lucas <lucaslyra47@gmail.com>
2023-04-25 05:11:16 -0300

    semana 3

---
+++ 2023/lab1/lab1q2.c
@@ -0,0 +1,124 @@
+/* Disciplina: Computacao Concorrente */
+/* Prof.: Silvana Rossetto */
+/* Módulo 1 - Laboratório: 1 */
+/* Lucas de Lyra Monteiro - 121039714*/
+/* Codigo: Multiplica por 2 todos os itens de uma array usando threads em C e a funcao que espera as threads terminarem */
+
+#include <stdio.h>
+#include <stdlib.h> 
+#include <pthread.h>
+#include <time.h>
+
+#define NTHREADS  2 //total de threads a serem criadas
+#define TAMANHOLISTA 10000 //tamanho da array
+
+//imprime uma array de doubles
+void imprime(double *array, int tamanhoArray) {
+  int i;
+  for (i=0; i < tamanhoArray; i++) {
+      printf("%d - %f\n", i, array[i]);
+  }
+}
+
+
+// preenche um array com números randômicos
+void preenche_lista (double *array, int tamanhoArray){
+  int i;
+  for(i=0; i < tamanhoArray; i++){
+      array[i] = i;
+  }
+}
+
+
+//cria a estrutura de dados para armazenar os argumentos da thread
+typedef struct {
+    int idThread, nThreads;
+    double *rlist;
+} t_Args;
+
+
+//função sequencial
+void multiplica_dois (double *array){
+    for (int i=0; i<TAMANHOLISTA; i++){
+        array[i] = array[i]*2;
+    }
+}
+
+
+//funcao executada pelas threads
+void *multiplica_dois_threads (void *arg) {
+    t_Args *args = (t_Args *) arg;
+    int i;
+    printf("Sou a thread %d de %d threads\n", args->idThread+1, args->nThreads);
+
+    for (i=TAMANHOLISTA/NTHREADS*args->idThread; i<(TAMANHOLISTA/NTHREADS)*(args->idThread+1); i++){
+        args->rlist[i] = args->rlist[i]*2;
+    }
+    free(arg); //aqui pode liberar a alocacao feita na main
+
+    pthread_exit(NULL);
+}
+
+
+// testa se dois doubles são iguais, vendo se o valor absoluto da diferença entre eles é maior que 10^-6
+int correto(double *arrayThreads, double *array){
+    for (int i = 0; i<TAMANHOLISTA; i++){
+        if (abs(array[i]-arrayThreads[i]) > 0.000001){ //se a diferença é maior que 10^-6 deu ruim
+            printf("Erro no item %d\n", i);
+            return 1;
+        }
+    }
+    printf("Passou em tudo! Yey :3\n");
+    return 0;
+}
+
+
+//funcao principal do programa
+int main() {
+    pthread_t tid_sistema[NTHREADS]; //identificadores das threads no sistema
+    double randomlist[TAMANHOLISTA], copyarray[TAMANHOLISTA]; //array com numeros aleatorios
+    int thread; //variavel auxiliar
+
+    t_Args *arg; //receberá os argumentos para a thread
+
+
+    srand(time(NULL));
+    preenche_lista(randomlist, TAMANHOLISTA); //preenche a array com numeros aleatorios
+
+    // copia os itens da array preenchida para uma outra array para testar depois
+    for (int i = 0; i<TAMANHOLISTA; i++){
+        copyarray[i] = randomlist[i];
+    }
+
+    //printf("%f%f", copyarray[0], randomlist[0]);
+    for(thread=0; thread<NTHREADS; thread++) {
+        printf("--Aloca e preenche argumentos para thread %d\n", thread+1);
+        arg = malloc(sizeof(t_Args));
+        if (arg == NULL) {
+            printf("--ERRO: malloc()\n"); exit(-1);
+        }
+        arg->idThread = thread; 
+        arg->nThreads = NTHREADS;
+        arg->rlist = randomlist;
+        
+        printf("--Cria a thread %d\n", thread+1);
+        if (pthread_create(&tid_sistema[thread], NULL, multiplica_dois_threads, (void*) arg)) {
+            printf("--ERRO: pthread_create()\n"); exit(-1);
+        }
+    }
+
+    //--espera todas as threads terminarem
+    for (thread=0; thread<NTHREADS; thread++) {
+        if (pthread_join(tid_sistema[thread], NULL)) {
+            printf("--ERRO: pthread_join() \n"); exit(-1); 
+      } 
+    }
+
+    multiplica_dois(copyarray); //multiplica a array copiada sequencialmente
+    correto(copyarray, randomlist); //testa se está correto
+
+    //imprime(randomlist, TAMANHOLISTA); teste antigo para ver se está correto
+
+    printf("--Thread principal terminou\n");
+    pthread_exit(NULL);
+}
---
+++ 2023/lab3/pi.java
@@ -0,0 +1,62 @@
+import java.lang.reflect.Array;
+import static java.lang.Math.*;
+/* Disciplina: Computacao Concorrente */
+/* Prof.: Silvana Rossetto */
+/* Laboratório: 3 */
+/* Codigo: Estendendo a classe Thread de Java */
+
+
+//--PASSO 1: cria uma classe que estende a classe Thread 
+
+ 
+//--classe do metodo main
+class piApproximation {
+    static final int NTHREADS = 8;
+    private static int NITERATIONS = 100000000;
+    private static double[] sum = new double[NTHREADS];
+
+    static class piApproximationWorker extends Thread {
+        private int id;
+        //--construtor
+        public piApproximationWorker(int id) { 
+           this.id = id;
+        }
+     
+        //--metodo executado pela thread
+        public void run() {
+            for (int i = id * NITERATIONS; i < (id + 1) * NITERATIONS; i++) {
+                double term = Math.pow(-1, i) / (2.0 * i + 1.0);
+                sum[id] += term;    
+            }
+            
+        }
+    }
+
+ 
+    public static void main (String[] args) {
+        double pi=0;
+        //--reserva espaço para um vetor de threads
+        Thread[] threads = new Thread[NTHREADS];
+        
+        //--PASSO 2: cria threads da classe que estende Thread
+        for (int i=0; i<threads.length; i++) {
+            threads[i] = new Thread(new piApproximationWorker(i));
+        }
+
+        //--PASSO 3: iniciar as threads
+        for (int i=0; i<threads.length; i++) {
+            threads[i].start();
+        }
+
+        //--PASSO 4: esperar pelo termino de todas as threads
+        for (int i=0; i<threads.length; i++) {
+            try { threads[i].join(); } 
+            catch (InterruptedException e) { return; }
+        }
+        for (int i = 0; i<sum.length; i++){
+            pi += sum[i];
+        }
+        pi = 4*pi;
+        System.out.println(pi); 
+    }
+}
